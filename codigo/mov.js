71ac98000066cfcdddd69244ec559f16542f02bd
Gabriel Eduardo Vilar <gabrieleduardo9393@gmail.com>
2025-05-22 11:20:56 -0300

    alinhamento
    foi alinhado algumas partes

--- codigo/mov.js
+++ codigo/mov.js
@@ -4,7 +4,7 @@ const contadorCarrinho = document.getElementById('contador-carrinho');
 var adicionados = Array.from(document.querySelectorAll('.carrinho')).slice(0, 8);
 adicionados.forEach(adicionado => {
     adicionado.addEventListener('click', function() {
-        adicionado.innerText = 'adicionado ❤ ';
+        adicionado.innerText = 'adicionado';
         adicionado.style.backgroundColor = 'green';
         contador++;
         contadorCarrinho.innerText = contador;
